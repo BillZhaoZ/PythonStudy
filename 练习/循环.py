2efb0e82f11786acea9265893d6fe7810c1582d2
BillZhaoZ <zzbillcumt@126.com>
2018-01-10 17:56:59 +0800

    循环

--- 练习/循环.py
+++ 练习/循环.py
@@ -1,7 +1,7 @@
 # 一江夜雨  Python学习  2018
 
 
-# words = ["你好", "我是", "你", "爸爸吧"]
+# words = ["你好", "我是", "你", "吗？？"]
 #
 # for s in words:
 #     print(s)
@@ -25,3 +25,21 @@ for n in range(2, 10):
         continue
 
     print("奇数" + str(n))
+
+# while 循环
+# 例子1
+i = 1
+
+while i <= 5:
+    print("hello world")
+    i += 1
+print("循环结束后的：i = %d" % i)
+
+# 例子2
+count = 0
+
+while count < 9:
+    print('The count is:', count)
+    count += 1
+
+print("Good bye!")
