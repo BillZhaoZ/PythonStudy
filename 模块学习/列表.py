58e1f970ea0fb65675c2425cb40c5efa33a8f7fb
BillZhaoZ <zzbillcumt@126.com>
2018-01-11 17:03:35 +0800

    列表

--- 模块学习/列表.py
+++ 模块学习/列表.py
@@ -10,3 +10,17 @@ print(list[1:3])  # 输出第二个至第三个元素
 print(list[2:])  # 输出从第三个开始至列表末尾的所有元素
 print(tinylist * 2)  # 输出列表两次
 print(list + tinylist)  # 打印组合的列表
+
+# 更新列表
+list[2] = "一江夜雨"
+
+print(list)
+
+# 删除元素
+del list[4]
+print(list)
+
+# 长度
+print(len(list))
+
+print(list[-1])
